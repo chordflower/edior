7b99dd62ecd611e8644e9e1eb554920e091ac1ba
carddamom <carddamom@tutanota.com>
2021-02-19 19:55:14 +0000

    build: Created the edior distribution file

---
+++ edior-editor/src/main/java/cc/chordflower/edior/App.java
@@ -0,0 +1,26 @@
+package cc.chordflower.edior;
+
+import org.apache.deltaspike.cdise.api.CdiContainer;
+import org.apache.deltaspike.cdise.api.CdiContainerLoader;
+import org.apache.deltaspike.cdise.api.ContextControl;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+public class App {
+
+  public static void main( String[ ] args ) {
+    SLF4JBridgeHandler.removeHandlersForRootLogger( );
+    SLF4JBridgeHandler.install( );
+    CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer( );
+    cdiContainer.boot( );
+    Runtime.getRuntime( ).addShutdownHook( new Thread( cdiContainer::shutdown ) );
+
+    // Starting the application-context enables use of @ApplicationScoped beans
+    ContextControl contextControl = cdiContainer.getContextControl( );
+    contextControl.startContext( ApplicationScoped.class );
+
+    // You can use CDI here
+  }
+
+}
